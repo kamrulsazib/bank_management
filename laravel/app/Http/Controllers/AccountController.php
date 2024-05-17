fdc47fc561da4596ec28bb5a99b8415edfa26740
rakibbin <rakibkhan1772@gmail.com>
2024-05-17 23:07:50 +0600

    rakib update

--- laravel/app/Http/Controllers/AccountController.php
+++ laravel/app/Http/Controllers/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Account;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AccountController extends Controller
 {
@@ -13,8 +14,8 @@ class AccountController extends Controller
      */
     public function index()
     {
-        $Account = Account::get();
-        return $this->sendResponse($Account,'Account Type Return fetched successfully');
+        $account = Account::orderBy('id','desc')->get();
+        return $this->sendResponse($account,'Account Return fetched successfully');
     }
 
     /**
@@ -30,38 +31,67 @@ class AccountController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'account_type_id' => 'required',
+            'account_number' => 'required',
+            'balance' => 'required',
+            'customer_id' => 'required',
+            'date' => 'required',
+            
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors(), 422);
+        }
+        $input = $request->all();
+        $account = Account::create($input);
+        return $this->sendResponse($account, 'Account created successfully!');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Account $account)
+    public function show(string $id)
     {
-        //
+        $account = Account::find($id);
+        return $this->sendResponse($account,'Account  Return fetched successfully');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Account $account)
+    public function edit(string $id)
     {
-        //
+        $account = Account::find($id);
+        return $this->sendResponse($account,'Account  Return fetched successfully');
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Account $account)
+    public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'account_type_id' => 'required',
+            'account_number' => 'required',
+            'balance' => 'required',
+            'customer_id' => 'required',
+            'date' => 'required',
+            
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors(), 422);
+        }
+        $input = $request->all();
+        $account = Account::create($input)->update($input);
+        return $this->sendResponse($account, 'Account Update successfully!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Account $account)
+    public function destroy(string $id)
     {
-        //
+        $account = Account::find($id)->delete();
+        return $this->sendResponse($account,'Account deleted successfully');
     }
 }
